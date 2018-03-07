654b0f9e1d9dafab2021ed9c80d52766a7a19d2c
Stephanie M. Gogarten <sdmorris@uw.edu>
2018-03-07 09:43:45 -0800

    moved testthat helper functions from R/ to tests/testthat/helper_

--- tests/testthat/helper_testData.R
+++ tests/testthat/helper_testData.R
@@ -1,14 +1,14 @@
 
 .testData <- function() {
     gds <- SeqVarTools:::.testSeqVarData()
-    sample.id <- seqGetData(gds, "sample.id")
+    sample.id <- SeqArray::seqGetData(gds, "sample.id")
     df <- data.frame(sample.id=sample.id,
                      sex=sample(c("M","F"), replace=TRUE, length(sample.id)),
                      age=rnorm(length(sample.id), mean=50, sd=10),
                      outcome=rnorm(length(sample.id), mean=10, sd=5),
                      status=rbinom(length(sample.id), size=1, prob=0.4),
                      stringsAsFactors=FALSE)
-    sampleData(gds) <- AnnotatedDataFrame(df)
+    sampleData(gds) <- Biobase::AnnotatedDataFrame(df)
     gds
 }
 
